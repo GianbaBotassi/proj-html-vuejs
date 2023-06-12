b01ae51f66f09dea37ab62982c8f430dfeb0d93f
GianbaBotassi <gianba89@gmail.com>
2023-06-12 13:02:08 +0200

    Issue with font awesome npm install, used cdn fa in index.html Done infotopbar

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -9,26 +9,58 @@ export default {
     <header>
         <section id="info-top-bar">
             <div class="cont">
-                <font-awesome-icon :icon="['fas', 'star']" />
+                <div>
+                    <span>
+                        <i class="fa-solid fa-clock"></i>
+                        <span>Open Hours: Mon - Sat - 9:00 - 18:00</span>
+                    </span>
+                </div>
+                <div>
+                    <span>
+                        <i class="fa-solid fa-phone"></i>
+                        <span>+1 *305( 1234/5678)</span>
+                    </span>
+                    <span>
+                        <i class="fa-solid fa-envelope"></i>
+                        <span>hello@example.com</span>
+                    </span>
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+                </div>
+                <div></div>
             </div>
 
         </section>
-        <i class="fa-solid fa-star"></i>
     </header>
 </template>
 
 <style lang="scss" scoped>
 @use 'styles/partials/variables.scss' as *;
+@use 'styles/partials/mixins.scss' as *;
 
 header {
 
 
 
     #info-top-bar {
-        // background-color: $abbey;
-        // color: $white;
-        // font-size: 0.8rem;
-        // height: 50px;
+        background-color: $abbey;
+        color: $white;
+        font-size: 0.7rem;
+        padding: 10px 0;
+
+        >div {
+            @include flex-content(between);
+
+            i {
+                margin-right: 5px
+            }
+
+            >div {
+                @include flex-content(center);
+                gap: 20px;
+            }
+        }
     }
 }
 </style>
