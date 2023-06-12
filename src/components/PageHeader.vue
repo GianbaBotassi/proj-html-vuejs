9f655d7f4ef035a599ccdb7fa86ed45aa6e1f1f3
GianbaBotassi <gianba89@gmail.com>
2023-06-12 12:29:24 +0200

    Install font awesome dependencies

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -6,7 +6,29 @@ export default {
 </script>
 
 <template>
-    <h1>Header</h1>
+    <header>
+        <section id="info-top-bar">
+            <div class="cont">
+                <font-awesome-icon :icon="['fas', 'star']" />
+            </div>
+
+        </section>
+        <i class="fa-solid fa-star"></i>
+    </header>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@use 'styles/partials/variables.scss' as *;
+
+header {
+
+
+
+    #info-top-bar {
+        // background-color: $abbey;
+        // color: $white;
+        // font-size: 0.8rem;
+        // height: 50px;
+    }
+}
+</style>
